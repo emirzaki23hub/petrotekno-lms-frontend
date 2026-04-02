3674e1f72ecde2c1f7c64900521fdb3251c66746
emir <emirzakimuhammad@gmail.com>
2026-04-02 18:39:31 +0700

    fix: disable image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "petroteknoapp-dev.sgp1.digitaloceanspaces.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "api.petroteknoapp.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
+    images: {
+        unoptimized: true,
+        remotePatterns: [{
+                protocol: "https",
+                hostname: "petroteknoapp-dev.sgp1.digitaloceanspaces.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "api.petroteknoapp.com",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
